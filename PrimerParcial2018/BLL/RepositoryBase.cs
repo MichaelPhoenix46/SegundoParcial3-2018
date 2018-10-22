1b4e4479374e256b83a05c62832058db932a9cee
Jose Michael Castillo Duarte <jose_castillo5@ucne.edu.do>
2018-10-22 15:21:18 -0400

    Modificaciones para segundo parcial

---
+++ PrimerParcial2018/BLL/RepositoryBase.cs
@@ -0,0 +1,109 @@
+﻿using PrimerParcial2018.DAL;
+using PrimerParcial2018.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial2018.BLL
+{
+    public class RepositorioBase<T> : IDisposable, IRepository<T> where T : class
+    {
+        internal Contexto _contexto;
+        public RepositorioBase(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public virtual T Buscar(int id)
+        {
+            T entity;
+            try
+            {
+                entity = _contexto.Set<T>().Find(id);
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return entity;
+
+        }
+
+        public virtual bool ELiminar(int id)
+        {
+            bool paso = false;
+
+            try
+            {
+                T Entity = _contexto.Set<T>().Find(id);
+                _contexto.Set<T>().Remove(Entity);
+
+                paso = _contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        public List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            List<T> Lista = new List<T>();
+            try
+            {
+                Lista = _contexto.Set<T>().Where(expression).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Lista;
+        }
+
+        public virtual bool Guardar(T entity)
+        {
+            bool paso = false;
+
+            try
+            {
+                if (_contexto.Set<T>().Add(entity) != null)
+                    paso = _contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        public virtual bool Modificar(T entity)
+        {
+            RepositorioBase<Vendedores> repositorio = new RepositorioBase<Vendedores>(new Contexto());
+            bool paso = false;
+            _contexto = new Contexto();
+            try
+            {
+                _contexto.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                paso = _contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+        public void Dispose()
+        {
+            _contexto.Dispose();
+        }
+
+    }
+}
